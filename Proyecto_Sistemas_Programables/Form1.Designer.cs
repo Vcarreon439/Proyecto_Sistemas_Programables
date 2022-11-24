c832a775a5535f688e5a867f3f2ac81f926a9731
Edgar Arguijo <70034525+Edgar-Arguijo@users.noreply.github.com>
2022-11-23 20:56:27 -0600

    .

--- Proyecto_Sistemas_Programables/Form1.Designer.cs
+++ Proyecto_Sistemas_Programables/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.lblPortDesc = new System.Windows.Forms.Label();
             this.cboPorts = new System.Windows.Forms.ComboBox();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnDetener = new System.Windows.Forms.Button();
             this.PicBoxVentiladorOff = new System.Windows.Forms.PictureBox();
             this.trcBrAmarillo = new System.Windows.Forms.TrackBar();
             this.btnEnviar = new System.Windows.Forms.Button();
@@ -77,7 +78,6 @@
             this.btnCerrar = new System.Windows.Forms.Button();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.PicBoxBarra = new System.Windows.Forms.PictureBox();
-            this.btnDetener = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tbConfig.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -109,6 +109,7 @@
             this.tabControl1.Controls.Add(this.tbConfig);
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.Location = new System.Drawing.Point(12, 37);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -186,6 +187,21 @@
             this.tabPage1.Size = new System.Drawing.Size(587, 364);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Actuadores y Sensores";
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnDetener.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDetener.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetener.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnDetener.Location = new System.Drawing.Point(95, 89);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(75, 23);
+            this.btnDetener.TabIndex = 44;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
             // 
             // PicBoxVentiladorOff
             // 
@@ -504,7 +520,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(331, 17);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.Size = new System.Drawing.Size(80, 13);
             this.label5.TabIndex = 14;
             this.label5.Text = "Nivel de Gas";
             // 
@@ -513,7 +529,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(98, 17);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.Size = new System.Drawing.Size(148, 13);
             this.label4.TabIndex = 13;
             this.label4.Text = "Humedad en el Ambiente";
             // 
@@ -522,7 +538,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(98, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.Size = new System.Drawing.Size(129, 13);
             this.label3.TabIndex = 12;
             this.label3.Text = "Humedad en la Tierra";
             // 
@@ -531,7 +547,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(331, 86);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 11;
             this.label2.Text = "Nivel de Luz";
             // 
@@ -540,7 +556,7 @@
             this.lblSenseGas.AutoSize = true;
             this.lblSenseGas.Location = new System.Drawing.Point(331, 40);
             this.lblSenseGas.Name = "lblSenseGas";
-            this.lblSenseGas.Size = new System.Drawing.Size(25, 13);
+            this.lblSenseGas.Size = new System.Drawing.Size(28, 13);
             this.lblSenseGas.TabIndex = 10;
             this.lblSenseGas.Text = "000";
             // 
@@ -549,7 +565,7 @@
             this.lblSenseRain.AutoSize = true;
             this.lblSenseRain.Location = new System.Drawing.Point(98, 39);
             this.lblSenseRain.Name = "lblSenseRain";
-            this.lblSenseRain.Size = new System.Drawing.Size(25, 13);
+            this.lblSenseRain.Size = new System.Drawing.Size(28, 13);
             this.lblSenseRain.TabIndex = 9;
             this.lblSenseRain.Text = "000";
             // 
@@ -558,7 +574,7 @@
             this.lblSenseSoil.AutoSize = true;
             this.lblSenseSoil.Location = new System.Drawing.Point(98, 183);
             this.lblSenseSoil.Name = "lblSenseSoil";
-            this.lblSenseSoil.Size = new System.Drawing.Size(25, 13);
+            this.lblSenseSoil.Size = new System.Drawing.Size(28, 13);
             this.lblSenseSoil.TabIndex = 8;
             this.lblSenseSoil.Text = "000";
             // 
@@ -567,7 +583,7 @@
             this.lblSensFoto.AutoSize = true;
             this.lblSensFoto.Location = new System.Drawing.Point(331, 109);
             this.lblSensFoto.Name = "lblSensFoto";
-            this.lblSensFoto.Size = new System.Drawing.Size(25, 13);
+            this.lblSensFoto.Size = new System.Drawing.Size(28, 13);
             this.lblSensFoto.TabIndex = 7;
             this.lblSensFoto.Text = "000";
             // 
@@ -576,7 +592,7 @@
             this.lblLevel.AutoSize = true;
             this.lblLevel.Location = new System.Drawing.Point(98, 106);
             this.lblLevel.Name = "lblLevel";
-            this.lblLevel.Size = new System.Drawing.Size(25, 13);
+            this.lblLevel.Size = new System.Drawing.Size(28, 13);
             this.lblLevel.TabIndex = 6;
             this.lblLevel.Text = "000";
             // 
@@ -585,7 +601,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(98, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.Size = new System.Drawing.Size(125, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Sensor de Precencia";
             // 
@@ -654,21 +670,6 @@
             this.PicBoxBarra.Size = new System.Drawing.Size(641, 31);
             this.PicBoxBarra.TabIndex = 17;
             this.PicBoxBarra.TabStop = false;
-            // 
-            // btnDetener
-            // 
-            this.btnDetener.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnDetener.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDetener.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDetener.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDetener.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btnDetener.Location = new System.Drawing.Point(95, 89);
-            this.btnDetener.Name = "btnDetener";
-            this.btnDetener.Size = new System.Drawing.Size(75, 23);
-            this.btnDetener.TabIndex = 44;
-            this.btnDetener.Text = "Detener";
-            this.btnDetener.UseVisualStyleBackColor = false;
-            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
             // 
             // frmPrincipal
             // 
